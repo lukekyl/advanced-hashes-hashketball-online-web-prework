e4e32b82edc733cf078022a73b23125d4950aab8
Kyle Luke <th.oldschool@gmail.com>
2019-08-12 04:35:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -225,7 +225,7 @@ def most_points_scored
           stats.each {|data, int|
             if data == :points && int > total
               
-              name = points[location][attribute].current
+              name = points[location][attribute]
               total = int
               
             end
